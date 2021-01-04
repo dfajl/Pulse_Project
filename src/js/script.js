eae1b1ad2a47024e6e229e29f5e3366b4b0e875e
dfajl <dfajl@mail.ru>
2021-01-04 20:39:28 +0300

    create carousel with JS

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,12 @@
-let number = 7; /* задание переменной */
-const pi = 3.14; /* задание постоянной */
+/* let number = 7;  задание переменной 
+const pi = 3.14;  задание постоянной 
 
 number = 4;
 
 let leftBorderwidth = 200;
+ */
+
+
 
 /* Типы данных :
 number
@@ -104,4 +107,23 @@ logging (9, 5);
  задание функций. как пример:калькулятор
 */
 
+/* ОБРРАТИ ВНИМАНИЕ НА ТО, ЧТО ПУТЬ К КАРТИНКЕ УКАЗЫВАЕТСЯ НЕСКОЛЬКО ИНАЧЕ. УРОК 3.12 */ 
 
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"> <img src="icons/left_arrow.svg"> </button>',
+        nextArrow: '<button type="button" class="slick-next"> <img src="icons/right_arrow.svg"> </button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                dots: true,
+                arrows:false
+                    }
+            }
+        ]
+
+      });
+  });
